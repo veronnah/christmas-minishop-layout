72b274674769d35f9296fcbac406cbd2665a7030
veronnah <yoo.veroon@gmail.com>
2021-12-27 14:56:11 +0100

    last sections added, responsive breakpoints, scroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,117 @@
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
 if (navToggle) {
     navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
+        navMenu.classList.add('show-menu');
     })
 }
 
 if (navClose) {
     navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
+        navMenu.classList.remove('show-menu');
     })
 }
 
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
-function scrollHeader(){
-    const header = document.getElementById('header')
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+function scrollHeader() {
+    const header = document.getElementById('header');
+    if (this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', scrollHeader);
 
-/*=============== NEW SWIPER ===============*/
 let newSwiper = new Swiper('.new-swiper', {
-    spaceBetween: 24, 
-    loop: true, 
-    slidesPerView: 'auto', 
+    spaceBetween: 24,
+    loop: true,
+    slidesPerView: 'auto',
     centeredSlides: true,
 
     pagination: {
         el: '.swiper-pagination',
-        dynamicBullets: true
-    }, 
+        dynamicBullets: true,
+    },
     breakpoints: {
         992: {
-            spaceBetween: 80, 
+            spaceBetween: 80,
         },
     }
 })
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+function scrollActive() {
+    const scrollY = window.pageYOffset;
 
-/*=============== SHOW SCROLL UP ===============*/
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id');
 
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
 
-/*=============== DARK LIGHT THEME ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+const animation = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 200,
+})
+
+animation.reveal('.home__img, .new__container, .footer__container');
+animation.reveal('.home__data', {
+    delay: 400
+});
+animation.reveal('.giving__content, .gift__card', {
+    interval: 100
+});
+animation.reveal('.celebrate__data, .message__form, .footer__img1', {
+    origin: 'left'
+});
+animation.reveal('.celebrate__img, .message__img, .footer__img2', {
+    origin: 'right'
+});
